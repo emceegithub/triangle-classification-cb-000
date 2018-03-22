cc895f4d7a5f7143b2215e6a7a6dec9e5e04e3ba
Mark Cruz <markchriscruz@yahoo.com>
2018-03-22 04:57:49 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,24 +8,21 @@ class Triangle
   end
   
   def kind
+    array = [@side1,@side2,@side3]
+    array = array.sort
     if ((@side1 == 0) ||  (@side2 == 0) || (@side1 == 0))
       raise TriangleError
-    elsif ((@side1 < 0) ||  (@side2 < 0) || (@side3 < 0))
-      raise TriangleError
+    elsif (@side1 < 0 ||  @side2 < 0 || @side3 < 0)
+      puts "negative side"
+      #raise TriangleError
+    elsif (array[2] >= array[0] + array[1])
+        return "tri ineq"
     elsif ((side1 == side2) && (side2 == side3))
       return :equilateral
     elsif ((side1 == side2) ||  (side2 == side3) || (side1 == side3))
       return :isosceles
     elsif (!(side1 == side2) &&  !(side2 == side3) && !(side1 == side3)) 
       return :scalene
-    elsif
-      array = [@side1,@side2,@side3]
-      array = array.sort
-      if (array[2] >= array[0] + array[1])
-        return "tri ineq"
-      else
-        # do nothing
-      end
     else
       # do nothing
     end
