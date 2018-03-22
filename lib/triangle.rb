7ccb7104236171b0ff5cf406f240b9798d916738
Mark Cruz <markchriscruz@yahoo.com>
2018-03-22 03:16:21 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,13 +12,30 @@ class Triangle
       return :equilateral
     elsif ((side1 == side2) ||  (side2 == side3) || (side1 == side3))
       return :isosceles
-    elsif (!(side1 == side2) &&  !(side2 == side3) && !(side1 == side3)) 
+    elsif ((side1 == side2) &&  (side2 == side3) && (side1 == side3)) 
       return :scalene
+    elsif ((side1 == 0) ||  (side2 == 0) || (side1 == 0))
+      return "zero side"
+    elsif ((side1 < 0) ||  (side2 < 0) || (side1 < 0))
+      return "negative side"
+    elsif
+      array = [@side1,@side2,@side3]
+      array = array.sort
+      if (array[2] >= array[0] + array[1])
+        return "tri ineq"
+      else
+        # do nothing
+      end
+      #return array[0].to_s + " " + array[1].to_s + " " + array[2].to_s
     else
-      #do nothing
+      # do nothing
     end
   end
   
   class TriangleError < StandardError
+    def message 
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
   end
+  
 end
