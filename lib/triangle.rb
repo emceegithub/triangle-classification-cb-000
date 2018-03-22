5e84abaad7cead3479dc14329ec24aae36d2308b
Mark Cruz <markchriscruz@yahoo.com>
2018-03-22 03:17:13 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
       return :equilateral
     elsif ((side1 == side2) ||  (side2 == side3) || (side1 == side3))
       return :isosceles
-    elsif ((side1 == side2) &&  (side2 == side3) && (side1 == side3)) 
+    elsif (!(side1 == side2) &&  !(side2 == side3) && !(side1 == side3)) 
       return :scalene
     elsif ((side1 == 0) ||  (side2 == 0) || (side1 == 0))
       return "zero side"
