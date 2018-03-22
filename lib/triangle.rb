b4469eb78ea1abdf000ecfdf5e550eac27f36e0f
Mark Cruz <markchriscruz@yahoo.com>
2018-03-22 04:44:57 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,9 +9,7 @@ class Triangle
   
   def kind
     if ((side1 == 0) ||  (side2 == 0) || (side1 == 0))
-      begin 
-        raise TriangleError
-      end
+      raise TriangleError
     elsif ((side1 == side2) && (side2 == side3))
       return :equilateral
     elsif ((side1 == side2) ||  (side2 == side3) || (side1 == side3))
